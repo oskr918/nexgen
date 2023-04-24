abe2f7278f1c1e71e1c72aa21730feebe82eea91
oscar <camamoblamientos@gmail.com>
2023-04-24 20:27:50 -0300

    agregue login

---
+++ src/pages/components/Login.js
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import styles from '../../styles/contacto.module.css'
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(email , password)
+  };
+
+  return (
+    <div className={styles.container}>
+        <form onSubmit={handleSubmit} className={styles.contactForm}>
+      <div>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={handleEmailChange}
+        />
+      </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={handlePasswordChange}
+        />
+      </div>
+      <button type="submit">Iniciar sesión</button>
+    </form>
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/pages/registrarse.js
@@ -0,0 +1,11 @@
+import Layout from './components/Layout'
+import Login from './components/Login'
+const registrarse = () => {
+    return (
+        <Layout>
+            <Login></Login>
+        </Layout>
+    )
+}
+
+export default registrarse
